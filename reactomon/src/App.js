356d202e9b339adb4fc57160db3dfb9a880fe95b
wory04 <voroseszterveronika@gmail.com>
2020-02-15 13:59:35 +0100

    add routes

--- reactomon/src/App.js
+++ reactomon/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import PokemonList from "./components/pokemon-list/PokemonList";
+import TypeList from "./components/type-list/TypeList";
+
 import "./App.css";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Reactomon</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path={["/", "/pokemons"]} component={PokemonList}
+          />
+          <Route path="/types" component={TypeList}
+          />
+        </div>
+      </Router>
     );
   }
 }
