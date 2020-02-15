096d7ad9a83a1f0611de600a5978abc04bb7b9af
wory04 <voroseszterveronika@gmail.com>
2020-02-15 18:00:25 +0100

    refactor App with hooks

--- reactomon/src/App.js
+++ reactomon/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import PokemonList from "./components/pokemon-list/PokemonList";
@@ -7,19 +7,17 @@ import PokemonDetail from "./components/pokemon-detail/PokemonDetail";
 
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path={["/", "/pokemons"]} component={PokemonList} />
-          <Route path="/types" component={TypeList} />
-          <Route path="/pokemon/:id" component={PokemonDetail} />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path={["/", "/pokemons"]} component={PokemonList} />
+        <Route path="/types" component={TypeList} />
+        <Route path="/pokemon/:id" component={PokemonDetail} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
