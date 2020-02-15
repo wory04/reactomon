cbe7c672842ac1b71550f8c343ac8c1b0ddeb6a6
wory04 <voroseszterveronika@gmail.com>
2020-02-15 23:42:50 +0100

    implement AppTheme component

---
+++ reactomon/src/context/Colors.js
@@ -0,0 +1,21 @@
+const AppTheme = {
+  turquoise: {
+    BackgroundColor: "#00a3a8",
+    PokemonCardBackgroundColor: "#00b58c",
+    ListBackgroundColor: "#d9f9d9",
+    LinkBackgroundColor: "#265fa5",
+    LinkColor: "#ffdc83",
+    PokemonNameColor: "#2f4858"
+  },
+
+  crimson: {
+    BackgroundColor: "#c25e5e",
+    PokemonCardBackgroundColor: "#93656e",
+    ListBackgroundColor: "#ffe4e1",
+    LinkBackgroundColor: "#530004",
+    LinkColor: "#f3eed9",
+    PokemonNameColor: "#574240"
+  }
+};
+
+export default AppTheme;
