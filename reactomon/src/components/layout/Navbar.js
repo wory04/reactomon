82044fbcf61197d8ab12b65330d0c8153ed87bdb
wory04 <voroseszterveronika@gmail.com>
2020-02-15 21:10:04 +0100

    add styled-components

--- reactomon/src/components/layout/Navbar.js
+++ reactomon/src/components/layout/Navbar.js
@@ -1,13 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import "./layout.css";
+
+const StyledLink = styled(Link)`
+  color: #ffdc83;
+  font-size: 18px;
+  font-weight: 600;
+  text-align: center;
+  margin: 10px 20px 40px 20px;
+  padding: 8px 16px;
+  background-color: #265fa5;
+  display: inline-block;
+  border-radius: 8px;
+  width: 150px;
+`;
+
+const PokemonLogo = styled.img`
+  width: 400px;
+  display: block;
+  padding: 10px;
+  margin: 20px auto;
+`;
 
 function Navbar() {
   return (
     <div className="navbar">
-      <h1>Pokemons</h1>
-      <Link to="/pokemons">Pokemons</Link> | <Link to="/types">Types</Link>
+      <PokemonLogo src="/pokemon-logo.png" alt="Pokemon" />
+      <StyledLink to="/pokemons">Pokemons</StyledLink>
+      <StyledLink to="/types">Types</StyledLink>
     </div>
   );
 }
--- reactomon/src/components/pokemon-card/PokemonCard.js
+++ reactomon/src/components/pokemon-card/PokemonCard.js
@@ -2,8 +2,29 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import "./PokemonCard.css";
+const PokemonImg = styled.img`
+  margin-top: 20px;
+  width: 60%;
+`;
+
+const PokemonName = styled.h3`
+  color: #2f4858;
+  font-family: "Courier New", Courier, monospace;
+`;
+
+const StyledPokemonCard = styled.div`
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  display: inline-block;
+  background-color: #00b58c;
+  margin: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+`;
 
 function PokemonCard(props) {
   const [id, setId] = useState("");
@@ -18,10 +39,10 @@ function PokemonCard(props) {
 
   return (
     <Link to={`/pokemon/${id}`}>
-      <div className="pokemon-card">
-        <img src={imageUrl} alt={props.name}></img>
-        <p>{props.name}</p>
-      </div>
+      <StyledPokemonCard className="pokemon-card">
+        <PokemonImg src={imageUrl} alt={props.name} />
+        <PokemonName>{props.name}</PokemonName>
+      </StyledPokemonCard>
     </Link>
   );
 }
