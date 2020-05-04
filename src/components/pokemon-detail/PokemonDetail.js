922c94b55cc579902b39517ee0d42114dceeb61c
wory04 <voroseszterveronika@gmail.com>
2020-05-04 17:24:02 +0200

    move styling from css to styled components

--- src/components/pokemon-detail/PokemonDetail.js
+++ src/components/pokemon-detail/PokemonDetail.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
-
-import "./PokemonDetail.css";
+import styled from "styled-components";
 
 import ThemeContext from "../../context/ThemeContext";
 import AppTheme from "../../context/Colors";
 import PokeBall from "../poke-ball/PokeBall";
+
+const StyledPokemonDetail = styled.div`
+  text-align: center;
+  padding: 40px;
+  width: 960px;
+  margin: auto;
+  border-radius: 20px;
+  position: relative;
+  & img:first-of-type {
+    padding: 20px;
+    width: 20%;
+  }
+`;
 
 function PokemonDetail(props) {
   const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${props.match.params.id}/`;
@@ -21,10 +33,10 @@ function PokemonDetail(props) {
   const currentTheme = AppTheme[theme];
 
   useEffect(() => {
-    axios.get(pokemonUrl).then(resp => {
+    axios.get(pokemonUrl).then((resp) => {
       setName(resp.data.name);
-      setAbilities(resp.data.abilities.map(e => e.ability.name));
-      setTypes(resp.data.types.map(e => e.type.name));
+      setAbilities(resp.data.abilities.map((e) => e.ability.name));
+      setTypes(resp.data.types.map((e) => e.type.name));
       setHeight(resp.data.height);
       setWeight(resp.data.weight);
       setImageUrl(resp.data.sprites.front_default);
@@ -32,10 +44,10 @@ function PokemonDetail(props) {
   }, [pokemonUrl]);
 
   return (
-    <div
+    <StyledPokemonDetail
       className="pokemon-detail"
       style={{
-        backgroundColor: `${currentTheme.ListBackgroundColor}`
+        backgroundColor: `${currentTheme.ListBackgroundColor}`,
       }}
     >
       <img src={imageUrl} alt={name} />
@@ -51,16 +63,16 @@ function PokemonDetail(props) {
         <span>{types.join(", ")}</span>
       </div>
       <PokeBall name={name} url={pokemonUrl} />
-    </div>
+    </StyledPokemonDetail>
   );
 }
 
 PokemonDetail.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-    })
-  })
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    }),
+  }),
 };
 
 export default PokemonDetail;
