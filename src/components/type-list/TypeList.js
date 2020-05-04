71ea579d9a09dbb05f063cf6cfffe43fbbd70e84
wory04 <voroseszterveronika@gmail.com>
2020-05-04 17:31:01 +0200

    move styling from css to styled components for TypeList

--- src/components/type-list/TypeList.js
+++ src/components/type-list/TypeList.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-
-import "./TypeList.css";
+import styled from "styled-components";
 
 import ThemeContext from "../../context/ThemeContext";
 import AppTheme from "../../context/Colors";
+
+const StyledTypeList = styled.div`
+  display: block;
+  padding: 40px;
+  width: 960px;
+  margin: auto;
+  border-radius: 20px;
+  & li {
+    list-style-type: none;
+    text-align: center;
+  }
+  & h2 {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+`;
 
 function TypeList() {
   const [pokemonTypes, setPokemonTypes] = useState([]);
@@ -13,25 +28,24 @@ function TypeList() {
   const currentTheme = AppTheme[theme];
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/type").then(resp => {
+    axios.get("https://pokeapi.co/api/v2/type").then((resp) => {
       setPokemonTypes(resp.data.results);
     });
   }, []);
 
   return (
-    <div
-      className="type-list"
+    <StyledTypeList
       style={{
-        backgroundColor: `${currentTheme.ListBackgroundColor}`
+        backgroundColor: `${currentTheme.ListBackgroundColor}`,
       }}
     >
       <h2>Pokemon types</h2>
       <ul>
-        {pokemonTypes.map(pokemonType => (
+        {pokemonTypes.map((pokemonType) => (
           <li key={pokemonType.name}>{pokemonType.name}</li>
         ))}
       </ul>
-    </div>
+    </StyledTypeList>
   );
 }
 
